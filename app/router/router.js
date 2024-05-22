1abe91f660956a8bc7fba6f1543e672e11e2186e
rasoolabdi <M0rd00r@yahoo.com>
2024-05-22 20:05:20 +0330

    initial graphql

--- app/router/router.js
+++ app/router/router.js
@@ -5,7 +5,10 @@ const redisClient = require("../utils/init_redis");
 const { DeveloperRoutes } = require("./user/developer.routes");
 const { AdminRoutes } = require("./admin/admin.routes");
 const { VerifyAccessToken, checkRole } = require("../middlewares/verifyAccessToken");
+const { graphqlHTTP } = require("express-graphql");
 const router = Router();
+const {graphQLSchema} = require("./../graphql/index.resolver");
+const { graphqlConfig } = require("../utils/graphql.config");
 
 (async() => {
     await redisClient.set("hi" , "hello");
@@ -18,6 +21,7 @@ router.use("/" , HomeRoutes);
 router.use("/user" , UserAuthRoutes);
 router.use("/developer" , DeveloperRoutes);
 router.use("/admin",VerifyAccessToken , AdminRoutes);
+router.use("/graphql" , graphqlHTTP(graphqlConfig))
 
 
 module.exports = {
---
+++ app/utils/graphql.config.js
@@ -0,0 +1,12 @@
+
+function graphqlConfig (req,res){
+    return {
+        schema: graphQLSchema,
+        graphiql: true,
+        context: {req,res}
+    }
+}
+
+module.exports = {
+    graphqlConfig
+}
