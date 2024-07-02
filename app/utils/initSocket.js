0250cfcdd8eeac3ec8de1d203dd779311099a219
rasoolabdi <M0rd00r@yahoo.com>
2024-07-02 07:29:19 +0330

    add initSocket

---
+++ app/utils/initSocket.js
@@ -0,0 +1,15 @@
+const socketIO = require("socket.io");
+
+
+function initialSocket(httpServer) {
+    const io = socketIO(httpServer , {
+        cors: {
+            origin: "*"
+        }
+    })
+    return io;
+};
+
+module.exports = {
+    initialSocket
+}
